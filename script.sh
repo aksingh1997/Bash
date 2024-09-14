e4cb8417d34342dedc71431298651c63f069acf9
Abhishek Kumar Singh <30554818+aksingh1997@users.noreply.github.com>
2024-09-14 12:06:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,9 +26,22 @@ sign 5
 sign -3
 sign 0
 
-#taking input from command line
+#taking input from linux
 echo first input from console $1
 echo second input from console $2
 
+#declare an array, take away all the even numbers and print them
+myList=(1 2 3 4 5 6)
+evenList=()
+for i in ${myList[@]}
+do
+	if [ $((i % 2)) -eq 0 ] ; then	#notice the gaps after square bracket and equation. Notice we dont use $i in (i % 2). It is extra wrapped by round bracket 
+		evenList+=($i)
+	fi
+done
 
+for number in ${evenList[@]}
+do
+	echo $number
+done
 
